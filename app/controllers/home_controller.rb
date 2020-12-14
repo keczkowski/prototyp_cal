67e539bf68d4710b2b40375cf6f11d728ebfbd88
Krzysztof Keczkowski <krzysztof.keczkowski@gmail.com>
2020-12-14 05:56:18 +0100

    poprawiono strone glowna i strony poboczne uzytkownika

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,48 @@ class HomeController < ApplicationController
     @servers = current_user.servers
     @payments = current_user.payments
     @additional_services = AdditionalService.all
-   end
+  end
+
+  def user_domains
+    @domains = current_user.domains
+  end
+
+  def user_payments
+    @payments = current_user.payments
+  end
+
+  def additional_services_list
+    @additional_services = AdditionalService.all
+  end
+
+  def server_list
+    @servers = current_user.servers
+  end
+
+  def new_user_server
+    @server = Server.new
+  end
+
+  def create_user_server
+    @server = Server.new(server_params)
+    @server.user = current_user
+    @server.paid = true
+
+    respond_to do |format|
+      if @server.save
+        format.html { redirect_to server_list_path, notice: 'Server was successfully created.' }
+        format.json { render :show, status: :created, location: @server }
+      else
+        format.html { render :new_user_server }
+        format.json { render json: @server.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def server_params
+    params.require(:server).permit(:name, :package_id)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,13 @@ Rails.application.routes.draw do
                                                sign_up: 'signup' }
 
   get "home/index"
+  get "oferty", to: 'home#oferty', as: "oferty"
+  get "domeny", to: 'home#user_domains', as: "user_domains"
+  get "platnosci", to: 'home#user_payments', as: "user_payments"
+  get "additional_services_list", to: 'home#additional_services_list', as: "additional_services_list"
+  get "server_list", to: 'home#server_list', as: "server_list"
+  get "new_user_server", to: 'home#new_user_server', as: "new_user_server"
+  post 'create_user_server', to: 'home#create_user_server', as: 'create_user_server'
   root to: 'home#index'
 
 end
