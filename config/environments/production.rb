66990d31dbc9471cf645d585fbd5daf53e3d9d93
Krzysztof Keczkowski <krzysztof.keczkowski@gmail.com>
2020-12-15 13:16:37 +0100

    sasas

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,7 @@ Rails.application.configure do
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
+  config.secret_key_base = 'blipblapblup'
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
